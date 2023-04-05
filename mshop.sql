3033d8fe929e4b86c94fa34ec6cc6b0dbfeb5504
Andrija2503 <94871751+Andrija2503@users.noreply.github.com>
2023-04-05 09:50:16 +0200

    Add files via upload

---
+++ mshop.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 25, 2023 at 11:59 AM
+-- Server version: 5.7.36
+-- PHP Version: 7.4.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mshop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `devices`
+--
+
+DROP TABLE IF EXISTS `devices`;
+CREATE TABLE IF NOT EXISTS `devices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `manufacturer` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `model` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `year` int(11) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ views/index.view.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>M shop</title>
+</head>
+<body>
+<nav class="navbar navbar-expand navbar-light bg-secondary d-flex justify-content-between p-2">
+    <a href="index.php" class="navbar-brand">M Shop</a>
+    <ul class="navbar-nav">
+        
+        <li class="nav-item"><a href="add_news.php" class="btn btn-outline-secondary">Dodaj Vijesti</a></li>
+        <li class="nav-item"><a href="" class="nav-link">link2</a></li>
+        <a href="logout.php" class="nav-link">Logout</a>
+    </ul>
+</nav>
+<div class="container p-0 mt-3">
+  <div class="m-0 mb-5 pb-1 bg-success text-white text-center rounded">
+    <h1 class="mb-2">Mobile Shop</h1> 
+  </div>
+</div>
+<div class="container">
+    <div class="row justify-content-between">
+        <div class="card mb-5" style="width: 20rem;">
+            <div class="card-body pb-1">
+                <h4 class="card-title">A51</h4>
+                <div class="d-flex justify-content-between">
+                <h6 class="card-subtitle mb-2 text-muted">Samsung</h6>
+                <h6 class="card-subtitle mb-2 text-muted">Year <small>2012</small></h6>
+                </div>
+                <p class="card-text">Some quick examcdsdcdsdcdsdc cdsd cdsd csdcdscdsdcdsd cdsd csd csd csple text to build on the card title and make up the bulk of the card's content.</p>
+                <p class="text-end"><small>Kreirano: 225-02-20 16:05h</small></p>
+            </div>
+            <div class="card-footer pb-2 d-flex justify-content-between">
+                <a href="#" class="btn btn-sm btn-primary">Izmeni</a>
+                <a href="#" class="btn btn-sm btn-danger">Obrisi</a>
+            </div>
+        </div>
+        
+    </div>
+</div>
+</body>
+</html>
